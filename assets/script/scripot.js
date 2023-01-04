f942672580b9bf94dd78efb35d5418f6d4c07519
RodolfoCRS <rodolfocrs.eng@gmail.com>
2023-01-04 14:49:37 -0300

    first commit

---
+++ assets/script/scripot.js
@@ -0,0 +1,13 @@
+const btnMobile = document.getElementById('btnmobile')
+        function toggleMenu () {
+            const nav = document.getElementById('nav')
+            nav.classList.toggle('active')
+        }
+        btnMobile.addEventListener('click', toggleMenu)
+
+
+
+        function alertaform() {
+            alert("Sua solicitação foi enviada com sucesso!                                                Em breve entraremos em contato.")
+        }
+
